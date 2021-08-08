eb6e5b5cf05957f6b406adc5a09f3491d49c9cce
Tobi <toah@gmx.net>
2021-08-08 22:22:12 +0200

    first Test

--- src/test/java/com/tobi/design/pattern/builder/impl/HeroImplTest.java
+++ src/test/java/com/tobi/design/pattern/builder/impl/HeroImplTest.java
@@ -1,9 +1,27 @@
 package com.tobi.design.pattern.builder.impl;
 
+import com.tobi.design.pattern.builder.Hero;
+import com.tobi.design.pattern.builder.HeroBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeroImplTest {
 
+    private HeroBuilder builder;
 
+    @BeforeEach
+    public void setUp(){
+        builder = new HeroBuilder();
+    }
+
+    @Test
+    void testBuilderWithName(){
+        Hero firstHero = builder.setName("Joke")
+        .build();
+
+        assertEquals("Joke",firstHero.getName());
+    }
 
 }
